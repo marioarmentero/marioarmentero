175ffe48d8d2cafa7df14e6905d718c3bd637819
marmentero <marmentero@seipsystems.com>
2021-11-08 22:09:27 +0100

    08990

--- aicogestnew/aicogestnew.Android/MainActivity.cs
+++ aicogestnew/aicogestnew.Android/MainActivity.cs
@@ -16,8 +16,11 @@ namespace aicogestnew.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+           // Plugin.CurrentActivity.CrossCurrentActivity.Current.Activity = this;
             LoadApplication(new App());
         }
+
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
--- aicogestnew/aicogestnew/Views/DocumentosScanner.xaml.cs
+++ aicogestnew/aicogestnew/Views/DocumentosScanner.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
@@ -12,9 +17,181 @@ namespace aicogestnew.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DocumentosScanner : ContentPage
     {
+        static string subscriptionKey = "fa68ae2ad1b944f1b083fae64abc9f54";
+        private const string READ_TEXT_URL_IMAGE = "https://intelligentkioskstore.blob.core.windows.net/visionapi/suggestedphotos/3.png";
+        static string endpoint = "https://aicoffiapi.cognitiveservices.azure.com/";
         public DocumentosScanner()
         {
             InitializeComponent();
+            //listView2.ItemTapped += ListitemTapped;
+
+            var tapFoto = new TapGestureRecognizer();
+            tapFoto.Tapped += async (s, e) =>
+            {
+                //Hacer una foto
+                try
+                {
+                    if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                    {
+                        await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                        return;
+                    }
+
+                    var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions()
+                    {
+                        Directory = "Test",
+                        SaveToAlbum = true,
+                        CompressionQuality = 75,
+                        CustomPhotoSize = 50,
+                        PhotoSize = PhotoSize.MaxWidthHeight,
+                        MaxWidthHeight = 2000,
+                        DefaultCamera = CameraDevice.Front,
+                        Name = $"{DateTime.UtcNow}.jpg"
+                    });
+
+                    if (file == null) return;
+                    imgChoosed.Source = ImageSource.FromStream(() => {
+                        var stream = file.GetStream();
+                        return stream;
+                    });
+
+                    var cliente = Authenticate(endpoint, subscriptionKey);
+
+                    var textHeaders = await cliente.ReadInStreamAsync(file.GetStream());
+
+                    string operationLocation = textHeaders.OperationLocation;
+
+                    const int numberOfCharsInOperationId = 36;
+                    string operationId = operationLocation.Substring(operationLocation.Length - numberOfCharsInOperationId);
+
+                    // Extract the text
+                    ReadOperationResult results;
+                   // Console.WriteLine($"Extracting text from URL file {Path.GetFileName(urlFile)}...");
+                   // Console.WriteLine();
+                    do
+                    {
+                        results = await cliente.GetReadResultAsync(Guid.Parse(operationId));
+                    }
+                    while ((results.Status == OperationStatusCodes.Running ||
+                        results.Status == OperationStatusCodes.NotStarted));
+
+                    var textUrlFileResults = results.AnalyzeResult.ReadResults;
+                    foreach (ReadResult page in textUrlFileResults)
+                    {
+                        foreach (Line line in page.Lines)
+                        {
+                            Console.WriteLine(line.Text);
+                        }
+                    }
+
+
+                    file.Dispose();
+
+                    /* var result = await Predict(file.GetStream());
+                     file.Dispose();
+                     List<Profes> MiListAdiv = new List<Profes>();
+
+
+                     foreach (Prediction va in result.predictions)
+                     {
+                         // lblResult.Text = lblResult.Text + "\n" + va.tagName + " Probabilidad " + va.probability.ToString("#0.##%"); ;
+                         if ((va.probability * 100) > 10)
+                         {
+                             Profes _Profes = new Profes
+                             {
+                                 tagname = va.tagName,
+                                 probability = va.probability.ToString("#0.##%") + " de Coincidencia"
+                             };
+                             MiListAdiv.Add(_Profes);
+                         }
+                     }
+                     listView2.ItemsSource = MiListAdiv;
+                     if (MiListAdiv.Count == 0)
+                     {
+                         await DisplayAlert("Info", "No hay resultados validos para la imagen", "OK");
+                     }*/
+
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("ERROR", ex.Message, "OK");
+                }
+
+
+
+            };
+            ImgPhoto.GestureRecognizers.Add(tapFoto);
+
+            //Imagen Search
+            var tapSearch = new TapGestureRecognizer();
+            tapSearch.Tapped += async (s, e) =>
+            {
+                //Galeria
+                await CrossMedia.Current.Initialize();
+                try
+                {
+
+                    var file = await Plugin.Media.CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+                    {
+                        PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium
+                    });
+                    if (file == null) return;
+                    imgChoosed.Source = ImageSource.FromStream(() => {
+                        var stream = file.GetStream();
+                        return stream;
+                    });
+
+
+                    /*
+                    var result = await Predict(file.GetStream());
+                    file.Dispose();
+                    List<Profes> MiListAdiv = new List<Profes>();
+
+                    foreach (Prediction va in result.predictions)
+                    {
+                        if ((va.probability * 100) > 10)
+                        {
+                            Profes _Profes = new Profes
+                            {
+                                tagname = va.tagName,
+                                probability = va.probability.ToString("#0.##%") + " de Coincidencia"
+                            };
+                            MiListAdiv.Add(_Profes);
+                        }
+
+                        //   lblResult.Text = lblResult.Text + "\n" + va.tagName + " Probabilidad " + va.probability.ToString("#0.##%");
+                    }
+                    
+                    listView2.ItemsSource = MiListAdiv;
+                    if (MiListAdiv.Count == 0)
+                    {
+                        await DisplayAlert("Info", "No hay resultados validos para la imagen", "OK");
+                    }*/
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("ERROR", ex.Message, "OK");
+
+                }
+
+            };
+            ImgSearch.GestureRecognizers.Add(tapSearch);
         }
+        private async void ListitemTapped(object sender, ItemTappedEventArgs e)
+        {
+            await DisplayAlert("No hay referencia para el item ", "123456", "OK");
+
+        }
+
+        public static ComputerVisionClient Authenticate(string endpoint, string key)
+        {
+            ComputerVisionClient client =
+              new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+              { Endpoint = endpoint };
+            return client;
+        }
+
+
+
     }
 }
